dd98ff22c050442892d7fcfdb8c474ac62664a99
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-09-22 19:35:44 -0300

    config api

---
+++ api/src/models/User.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+
+  sequelize.define('User', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+  },
+  {
+    timestamps: false
+  });
+};
