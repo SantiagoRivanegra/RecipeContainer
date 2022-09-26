805df28c3b3685bbad138a1a6d81cc1fc0a6e9dd
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-09-26 19:49:07 -0300

    all first user routes

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -2,4 +2,34 @@ const { Router } = require('express')
 
 const router = Router()
 
+// //Get Users for Admin
+// router.get('/', getUser)
+
+// //Get User Profile
+// router.get('/profile/:idUser', getUserProfile)
+
+// //Get User Recipe
+// router.get('/recipe/:idUser', getUserRecipe)
+
+// //Get all Comments
+// router.get('/commen/:idUser', getUserComment)
+
+// //Get all Likes
+// router.get('/likes/:idUser', getUserLike)
+
+// //Post User
+// router.post('/', postUser)
+
+// //Delete User
+// router.delete('/:idUser', deleteUser)
+
+// //Put Password
+// router.put('/password/:idUser', putPassword)
+
+// //Put Username
+// router.put('/username/:idUser', putUsername)
+
+// //Put Avatar
+// router.put('/avatar/:idUser', putAvatar)
+
 module.exports = router;
