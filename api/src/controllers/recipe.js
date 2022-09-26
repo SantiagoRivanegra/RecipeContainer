7d5da080fc44408077846e742d451c8966b11ba1
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-09-26 14:49:07 -0300

    random, name route

--- api/src/controllers/recipe.js
+++ api/src/controllers/recipe.js
@@ -10,8 +10,28 @@ const getApiInfo = async() =>{
         description: r.strInstructions,
         image: r.strMealThumb,
         videos: r.strYoutube,
-        ingredients: r.strIngredient1,
-        area: r.area,
+        ingredients1: r.strIngredient1 ? r.strIngredient1 : "",
+        ingredients2: r.strIngredient2 ? r.strIngredient2 : "",
+        ingredients3: r.strIngredient3 ? r.strIngredient3 : "",
+        ingredients4: r.strIngredient4 ? r.strIngredient4 : "",
+        ingredients5: r.strIngredient5 ? r.strIngredient5 : "",
+        ingredients6: r.strIngredient6 ? r.strIngredient6 : "",
+        ingredients7: r.strIngredient7 ? r.strIngredient7 : "",
+        ingredients8: r.strIngredient8 ? r.strIngredient8 : "",
+        ingredients9: r.strIngredient9 ? r.strIngredient9 : "",
+        ingredients10: r.strIngredient10 ? r.strIngredient10 : "",
+        ingredients11: r.strIngredient11 ? r.strIngredient11 : "",
+        ingredients12: r.strIngredient12 ? r.strIngredient12 : "",
+        ingredients13: r.strIngredient13 ? r.strIngredient13 : "",
+        ingredients14: r.strIngredient14 ? r.strIngredient14 : "",
+        ingredients15: r.strIngredient15 ? r.strIngredient15 : "",
+        ingredients16: r.strIngredient16 ? r.strIngredient16 : "",
+        ingredients17: r.strIngredient17 ? r.strIngredient17 : "",
+        ingredients18: r.strIngredient18 ? r.strIngredient18 : "",
+        ingredients19: r.strIngredient19 ? r.strIngredient19 : "",
+        ingredients20: r.strIngredient20 ? r.strIngredient20 : "",
+        measure1: r.strMeasure1 ? r.strMeasure1 : "",
+        area: r.strArea,
         category: r.strCategory,
         tags: r.strTags
       }
@@ -35,10 +55,84 @@ const getRecipes = async(req, res) => {
   const apiInfo = await getApiInfo()
   const dbInfo = await getDbInfo()
   const recipes = apiInfo.concat(dbInfo)
-  res.status(200).send(recipes)
   return recipes
+}
+
+const getRandomRecipe = async(req, res) => {
+    const apiUrl = await axios.get('https://www.themealdb.com/api/json/v1/1/random.php')
+    const random = await apiUrl.data.meals.map(r => {
+      return {
+        recipe_id: r.idMeal,
+        name_recipe: r.strMeal,
+        description: r.strInstructions,
+        image: r.strMealThumb,
+        videos: r.strYoutube,
+        ingredients1: r.strIngredient1 ? r.strIngredient1 : "",
+        ingredients2: r.strIngredient2 ? r.strIngredient2 : "",
+        ingredients3: r.strIngredient3 ? r.strIngredient3 : "",
+        ingredients4: r.strIngredient4 ? r.strIngredient4 : "",
+        ingredients5: r.strIngredient5 ? r.strIngredient5 : "",
+        ingredients6: r.strIngredient6 ? r.strIngredient6 : "",
+        ingredients7: r.strIngredient7 ? r.strIngredient7 : "",
+        ingredients8: r.strIngredient8 ? r.strIngredient8 : "",
+        ingredients9: r.strIngredient9 ? r.strIngredient9 : "",
+        ingredients10: r.strIngredient10 ? r.strIngredient10 : "",
+        ingredients11: r.strIngredient11 ? r.strIngredient11 : "",
+        ingredients12: r.strIngredient12 ? r.strIngredient12 : "",
+        ingredients13: r.strIngredient13 ? r.strIngredient13 : "",
+        ingredients14: r.strIngredient14 ? r.strIngredient14 : "",
+        ingredients15: r.strIngredient15 ? r.strIngredient15 : "",
+        ingredients16: r.strIngredient16 ? r.strIngredient16 : "",
+        ingredients17: r.strIngredient17 ? r.strIngredient17 : "",
+        ingredients18: r.strIngredient18 ? r.strIngredient18 : "",
+        ingredients19: r.strIngredient19 ? r.strIngredient19 : "",
+        ingredients20: r.strIngredient20 ? r.strIngredient20 : "",
+        measure1: r.strMeasure1 ? r.strMeasure1 : "",
+        area: r.strArea,
+        category: r.strCategory,
+        tags: r.strTags
+      }
+    })
+     res.status(200).json(random)
+}
+
+const getRecipeByName = async(req, res) => {
+  const { name } = req.params
+  try {
+    // const recipe = await getRecipes()
+    // if(!name){
+    //   res.status(404).send('nope')
+    // } else {
+    //   const rec = recipe.filter(rec => rec.name_recipe.toLowerCase() === name.toLowerCase())
+    //   rec.length ?
+    //   res.status(200).send(rec)
+    //   :
+    //   console.log(recipe) 
+    //   res.status(404).send('nopee')
+    // }
+    if(name){
+      const apiUrl = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${name}`)
+      if(apiUrl.data.meals){
+      const recipeName = await apiUrl.data.meals.map(r => {
+        return {
+          recipe_id: r.idMeal,
+          name_recipe: r.strMeal,
+          description: r.strInstructions,
+        }
+      })
+      res.status(200).json(recipeName)    
+    } else {
+      res.status(404).json('No exist')
+    }
+    }
+  } catch (error) {
+    console.log(error)
+  }
+
 }
 
 module.exports = {
   getRecipes,
+  getRandomRecipe,
+  getRecipeByName
 }
--- api/src/routes/recipe.js
+++ api/src/routes/recipe.js
@@ -1,9 +1,15 @@
 const { Router } = require('express')
-const { getRecipes } = require('../controllers/recipe')
+const { getRecipes, getRandomRecipe, getRecipeByName } = require('../controllers/recipe')
 
 const router = Router()
 
 //Get recipes
 router.get('/', getRecipes)
+
+//Get Random
+router.get('/random', getRandomRecipe)
+
+//Get by Name
+router.get('/name/:name', getRecipeByName)
 
 module.exports = router;
