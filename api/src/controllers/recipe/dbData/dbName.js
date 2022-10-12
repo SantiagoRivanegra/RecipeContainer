8447c123465949f1b1179d93ef17eb0eb4c30cab
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-10-12 15:56:21 -0300

    getName with names with space between

--- api/src/controllers/recipe/dbData/dbName.js
+++ api/src/controllers/recipe/dbData/dbName.js
@@ -11,18 +11,24 @@ const dbName = async(name) => {
       const namesContainer = []
      for (let i = 0; i < names.length; i++) {
        const namesArray = names[i].name.split(' ')
-       //console.log(namesArray)
         if(namesArray.length === 1) {
            if(namesArray[0] === name)
+           //console.log(namesArray[0] + '-' + name)
             namesContainer.push(namesArray[0])
          } else if(namesArray.length > 1){
+          console.log((namesArray.join(' ')) + '-' + name)
             for (let i = 0; i < namesArray.length; i++) {
               if(namesArray[i] === name){
                 namesContainer.push(namesArray.join(' '))
               }
+              if (namesArray.join(' ') === name){
+                namesContainer.push(namesArray.join(' '))
+                break;
+              }
             }
          }
        }
+       console.log(namesContainer)
        return namesContainer 
   }
 };
