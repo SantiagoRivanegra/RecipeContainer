862a3f708a0fb638bc0ba1cc31574f15c61684fa
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-10-15 13:49:09 -0300

    back tags finish

--- api/src/controllers/recipe/tags.js
+++ api/src/controllers/recipe/tags.js
@@ -1,40 +1,8 @@
-// const axios = require('axios')
-
-// const tags = async(req, res) => {
-//   const differentTabelsNotYetSeparated = []
-//   for (let i = 65; i < 70; i++) {   
-//     let letra = String.fromCodePoint(i).toLowerCase()
-//     const byLetter = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letra}`)
-//     if(byLetter.data.meals && byLetter.data.meals !== null){
-//       const recipeLetter = await byLetter.data.meals.map(r => {
-//         return {
-//           tags: r.strTags,
-//         }
-//       })
-//       const set = new Set()
-//       for (let i = 0; i < recipeLetter.length; i++)
-//       if(recipeLetter[i].tags !== null && recipeLetter[i].tags !== ''){
-//         if(set.has(recipeLetter[i].tags) === false){
-//           set.add(recipeLetter[i].tags)
-//        }
-//       }
-//       for (let item of set){
-//       differentTabelsNotYetSeparated.push(item)
-//       }
-//     }
-//   }
-//   console.log(differentTabelsNotYetSeparated)
-// }
-
-// module.exports = {
-//   tags
-// }
-
 const axios = require('axios')
 
 const tags = async(req, res) => {
   const differentTabelsNotYetSeparated = []
-  for (let i = 65; i < 90; i++) {   
+  for (let i = 65; i < 91; i++) {   
     let letra = String.fromCodePoint(i).toLowerCase()
     const byLetter = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?f=${letra}`)
     if(byLetter.data.meals && byLetter.data.meals !== null){
@@ -68,7 +36,7 @@ const tags = async(req, res) => {
   for (let item of set123){
     containerDifferentTags.push(item)
   }
-  console.log(containerDifferentTags)
+  res.status(200).json(containerDifferentTags)
 }
 
 module.exports = {
