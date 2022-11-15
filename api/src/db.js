bb299ea73aaa1068e65e632994c3c080ece3e98f
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2022-11-15 10:15:08 -0300

    new act

--- api/src/db.js
+++ api/src/db.js
@@ -35,7 +35,7 @@ const {
 //         { logging: false, native: false }
 //       )
 
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(DB_DEPLOY,{
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
