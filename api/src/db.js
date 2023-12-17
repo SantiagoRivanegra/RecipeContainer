b297bdda2789ee61cab9d33e88a69dd1069471e4
Santiago Rivanegra <santiagorivanegra@hotmail.com>
2023-12-17 17:00:20 -0300

    test deploy

--- api/src/db.js
+++ api/src/db.js
@@ -35,8 +35,7 @@ const {
 //         { logging: false, native: false }
 //       )
 
-//const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-  const sequelize = new Sequelize(DB_DEPLOY, {
+  const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
